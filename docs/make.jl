26068ab867f92819c904917d469b4b9d5130096f
Thibaut Lacroix <57836508+tfmlaX@users.noreply.github.com>
2024-01-10 17:33:04 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,8 +1,6 @@
 using Documenter
 using MPSDynamics
 
-#push!(LOAD_PATH,"../include/")
-
 makedocs(
     clean   = true,
     doctest = true,
@@ -14,19 +12,6 @@ makedocs(
 )
 
 deploydocs(
-    repo = "git@github.com:angusdunnett/MPSDynamics.git",
+    repo = "github.com/angusdunnett/MPSDynamics.git",
     devurl = "docs"
 )
-
-# bitbucket = "/home/angus/Documents/Julia/angus-dunnett.bitbucket.io/"
-# build = "/home/angus/Documents/Julia/mps2.0/docs/build/"
-
-# Base.Filesystem.cp(joinpath(build, "assets"), joinpath(bitbucket,"assets"), force=true)
-# Base.Filesystem.cp(joinpath(build,"index.html"), joinpath(bitbucket,"index.html"), force=true)
-# Base.Filesystem.cp(joinpath(build,"search"), joinpath(bitbucket,"search"), force=true)
-# Base.Filesystem.cp(joinpath(build,"search_index.js"), joinpath(bitbucket,"search_index.js"), force=true)
-# cd(bitbucket)
-# pwd()
-# run(Cmd(`git commit -a -m "auto-commit"`))
-# run(Cmd(`git push`))
-
