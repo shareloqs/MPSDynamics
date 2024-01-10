26a0fbd01f7f8647206d42942d8471587aae9490
Thibaut Lacroix <57836508+tfmlaX@users.noreply.github.com>
2024-01-10 18:26:13 +0100

    Update mpsBasics.jl

--- src/mpsBasics.jl
+++ src/mpsBasics.jl
@@ -464,7 +464,7 @@ Return the element of the MPS `A` for the set of physical states `el...`
 
 # Examples
 
-```jldoctest
+```julia-repl
 julia> A = chainmps(6, [2,4], 1);
 
 julia> elementmps(A, 1, 2, 1, 1, 1, 1)
